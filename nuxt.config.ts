7d26e93dd28dfa7df7fa4ce7e1af6d4369bd6a21
Hamza-Alchbli <hamzaomenxx@gmail.com>
2026-03-11 23:41:13 +0100

    add i18n in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/i18n', 'nuxt-studio']
+  modules: ['@nuxt/content', '@nuxtjs/i18n', 'nuxt-studio'],
+  i18n: {
+    defaultLocale: 'en',
+  },
 })
